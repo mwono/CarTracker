932d2d4cc060c6606f7106471ae4237e78ddd7cf
Christian Dorado <chrisdoradotf2@gmail.com>
2019-03-30 10:39:21 -0700

    added login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainPageComponent } from './main-page/main-page.component';
 import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent },
   { path: 'Register', component: RegisterComponent},
+  { path: 'Login', component: LoginComponent},
   { path: '**', redirectTo: 'Register' }
 ];
 
