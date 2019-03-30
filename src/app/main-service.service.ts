dae04e4fe7e163ae81955ffeea30bc08b284ed77
mwono <michaelwono@gmail.com>
2019-03-30 10:14:44 -0700

    Added error handling

--- src/app/main-service.service.ts
+++ src/app/main-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { User } from './User';
 
 @Injectable({
@@ -16,19 +16,54 @@ import { User } from './User';
 export class MainService {
   constructor(private http: HttpClient) { }
 
-  SendUpdate(lot: string): Observable<any> {
-    return this.http.post<any>('/sendUpdate', lot);
+  SendUpdate(parkingLot: string, cap: number): Observable<any> {
+    return this.http.post<any>('/sendUpdate', { lot : parkingLot , capacity : cap })
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
-  GetRegistered(lot: string): Observable<any[]> {
-    return this.http.post<any[]>('/getActiveRegistered', lot);
+  GetRegistered(parkingLot: string): Observable<any[]> {
+    return this.http.post<any[]>('/getActiveRegistered', { lot : parkingLot })
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
-  GetUnregistered(lot: string): Observable<any[]> {
-    return this.http.post<any[]>('/getActiveNotReg', lot);
+  GetUnregistered(parkingLot: string): Observable<any[]> {
+    return this.http.post<any[]>('/getActiveNotReg', { lot : parkingLot })
+    .pipe(
+      catchError(this.handleError)
+    );
   }
 
-  GetCapacity(lot: string): Observable<any> {
-    return this.http.post<any>('/getCapacity', lot);
+  GetCapacity(parkingLot: string): Observable<any> {
+    return this.http.post<any>('/getCapacity', { lot : parkingLot })
+    .pipe(
+      catchError(this.handleError)
+    );
   }
+
+  GetCurrentLot(parkingLot: string): Observable<any> {
+    return this.http.post('getLotInfo', { lot: parkingLot})
+    .pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
--- src/app/register.service.ts
+++ src/app/register.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { User } from './User';
 
 @Injectable({
@@ -12,6 +12,25 @@ export class RegisterService {
   constructor(private http: HttpClient) { }
 
   CreateNewUser(user: User): Observable<any> {
-    return this.http.post<any>('/register', user);
+    return this.http.post<any>('/register', user)
+    .pipe(
+      catchError(this.handleError)
+    );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
