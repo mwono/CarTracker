e7e04e1eb991c77fdd96aeb072437b9b981bd4c6
Christian Dorado <chrisdoradotf2@gmail.com>
2019-03-30 14:04:28 -0700

    edited format for pages with bootstrap

---
+++ src/app/login.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http:HttpClient) { }
+
+  loginUser() {
+      
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../login.service';
+import { User } from '../User';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    user: User = {
+        phone: 0,
+        plate: '',
+        name: '',
+    }
 
-  constructor() { }
+    constructor(private serv: LoginService) { }
 
-  ngOnInit() {
-  }
+    Login() {
+        this.serv.loginUser
+    }
+
+    ngOnInit() {
+    }
 
 }
